e3b33462466127e049252f0621edf4be56f5184e
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-03-15 22:02:57 +0530

    updated

---
+++ vesttech/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+
+const App = () => {
+  return (
+    <>
+      <div>xd</div>
+    </>
+  );
+}
+
+export default App;
