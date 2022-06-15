e2607b55fec10e8033f6679f1a3f22e22cdc8114
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-15 22:49:59 +0700

    feat: User model

---
+++ vesttech/server/src/Models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const User = new mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		username: { type: String, required: true },
+		email: { type: String, required: true },
+		password: { type: String, required: true },
+	},
+	{ timestamps: true }
+)
+
+module.exports = mongoose.model('users', User)
