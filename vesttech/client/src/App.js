37d15691c96b3f5d27a658539e85b2ad8d11e03e
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-06-16 21:01:23 +0530

    Updated

--- vesttech/client/src/App.js
+++ vesttech/client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import {BrowserRouter as Router,Route, Routes} from 'react-router-dom';
 import Home from './pages/Home.js';
-import Account from './pages/Account.js';
 import './App.css';
 
 const App = () => {
   return (
     <Routes>
       <Route exact path='/' element={<Home />} />
-      <Route exact path="/account" element={ <Account />} />
     </Routes>
   );
 }
