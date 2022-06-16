8e6f31d5406b4e76d776d80c5aff00d92d6681dc
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-16 07:27:43 +0700

    feat: Validate sign in

--- vesttech/server/src/Routes/Auth.js
+++ vesttech/server/src/Routes/Auth.js
@@ -8,11 +8,12 @@ const VerifyJWT = require('../Middlewares/VerifyJWT')
 
 // Validation
 const SignUpValidation = require('../Validation/Auth/SignUp')
+const SignInValidation = require('../Validation/Auth/SignIn')
 
 const Router = express.Router()
 
 Router.post('/signup', SignUpValidation(), SignUp)
-Router.post('/signin', SignIn)
+Router.post('/signin', SignInValidation(), SignIn)
 Router.get('/user', VerifyJWT, GetUserData)
 
 module.exports = Router
