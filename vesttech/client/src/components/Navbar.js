c24a5d29a4065ddb36ee91c4e09c9adb7e0c1718
Potato-29 <csaprayas@gmail.com>
2022-06-18 13:43:04 +0530

    #15 Create a navbar fix: Created a responsive navbar

--- vesttech/client/src/components/Navbar.js
+++ vesttech/client/src/components/Navbar.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import "../styles/Navbar.css";
+import Logo from '../images/bitcoin-2.png';
+import { Navbar as BsNavbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+
 
 const Navbar = () => {
   return (
-    <>
-      Navbar
-    </>
+      <div className='navbar'>
+        <BsNavbar className="" expand='lg'>
+          <div className='logo'>
+              <img
+                alt="vesttechLogo"
+                src={Logo}
+                className="vesttech-logo"
+              />{' '}
+              <h2>VestTech</h2>
+          </div>
+          <BsNavbar.Toggle className='nav-toggle navbar-dark' aria-controls='basic-navbar-nav' />
+          <BsNavbar.Collapse className='nav-collapse justify-content-end' id='basic-navbar-nav'>
+          <ul className='nav-list'>
+              <li><Nav.Link href="#" eventKey="2" className='nav-item'>Home</Nav.Link></li>
+              <li><Nav.Link href="#" className='nav-item'>About Us</Nav.Link></li> 
+              <li><Nav.Link href="#" className='nav-item'>Invest</Nav.Link></li>
+              <li><Nav.Link href="#" className='nav-item'>FaQ</Nav.Link></li>
+              <li><Nav.Link href="#" className='nav-item'>Contact</Nav.Link></li>       
+            </ul>
+          </BsNavbar.Collapse>
+          
+            
+        </BsNavbar>
+      </div>
   )
 }
 
